8e83f2b7fca6822f969a1e16582575043cf20375
Konstantinos <kamazgal@csd.auth.gr>
2021-03-11 16:29:04 +0200

    Added DFS implementation.

--- Maze.py
+++ Maze.py
@@ -2,6 +2,8 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from matplotlib.path import Path
+import matplotlib.patches as patches
 
 
 def color(RGB) -> int:
@@ -20,7 +22,7 @@ def readImage2Array():
     im = Image.open('in.png').convert('RGB')  # type: Image.Image
     ROWS, COLUMNS = im.size
 
-    A = np.zeros((COLUMNS, ROWS))
+    A = np.zeros((COLUMNS, ROWS), dtype=int)
     start = (0, 0)
     finish = (0, 0)
     for i in range(ROWS):
@@ -34,33 +36,43 @@ def readImage2Array():
     return A, start, finish
 
 
-def plotMaze():
+def plotMaze(A, path=[]):
     cmap = colors.ListedColormap(['white', 'black', 'green', 'red'])
     fig, ax = plt.subplots()
     ax.imshow(A, cmap=cmap)
 
     # Major ticks
-    ax.set_xticks(np.arange(0, ROWS, 1))
-    ax.set_yticks(np.arange(0, COLUMNS, 1))
+    ax.set_xticks(np.arange(0, COLUMNS, 1))
+    ax.set_yticks(np.arange(0, ROWS, 1))
 
     # Labels for major ticks
     # ax.set_xticklabels(np.arange(1, ROWS+1, 1))
     # ax.set_yticklabels(np.arange(1, COLUMNS + 1, 1))
 
     # Minor ticks
-    ax.set_xticks(np.arange(-.5, ROWS, 1), minor=True)
-    ax.set_yticks(np.arange(-.5, COLUMNS, 1), minor=True)
+    ax.set_xticks(np.arange(-.5, COLUMNS, 1), minor=True)
+    ax.set_yticks(np.arange(-.5, ROWS, 1), minor=True)
     ax.grid(which='minor', axis='both', linestyle='-', color='k', linewidth=2)
     ax.xaxis.tick_top()
-    plt.savefig('out.png', bbox_inches='tight', dpi=300)
-    # plt.show()
+    verts = [(t[1], t[0]) for t in path]
+
+    codes = [Path.MOVETO] + [Path.LINETO] * (len(verts) - 1)
+    print(codes)
+
+    path = Path(verts, codes)
+
+    patch = patches.PathPatch(path, facecolor='none', lw=2, edgecolor='yellow', zorder=3, )
+    ax.add_patch(patch)
+
+    # plt.savefig('out.png', bbox_inches='tight', dpi=300)
+    plt.show()
 
 
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, parents=None):
         self.data = data
         self.children = []
-        self.parents = []
+        self.parents = [parents]
 
     def add_child(self, obj):
         self.children.append(obj)
@@ -69,7 +81,7 @@ class Node(object):
         self.parents.append(obj)
 
 
-def getValidStates(Array, currentPos: tuple):
+def getValidStates(Array, currentPos):
     """
     A valid move is consider:
         UP   : A[i-1][j]
@@ -81,34 +93,78 @@ def getValidStates(Array, currentPos: tuple):
     Returns a tuple int Array containing the possible Moves (Line,Collum)
     :rtype: tuple
     """
+    ROWS = len(Array)
+    COLUMNS = len(Array[0])
+
     i, j = currentPos
     L = []
-    if i != 0:  # UP   : A[i-1][j]
-        if A[i - 1][j] != 1:  # Not a WALL cell
-            L.append(i - 1, j)
-
-    if i != ROWS - 1:  # DOWN : A[i+1][j]
-        if A[i + 1][j] != 1:  # Not a WALL cell
-            L.append(i + 1, j)
 
     if j != 0:  # LEFT : A[i][j-1]
-        if A[i][j - 1] != 1:  # Not a WALL cell
-            L.append(i, j - 1)
+        if Array[i][j - 1] != 1:  # Not a WALL cell
+            L.append((i, j - 1))
+
+    if i != 0:  # UP   : A[i-1][j]
+        if Array[i - 1][j] != 1:  # Not a WALL cell
+            L.append((i - 1, j))
 
     if j != COLUMNS - 1:  # RIGHT: A[i][j+1]
-        if A[i][j + 1] != 1:  # Not a WALL cell
-            L.append(i, j + 1)
+        if Array[i][j + 1] != 1:  # Not a WALL cell
+            L.append((i, j + 1))
+
+    if i != ROWS - 1:  # DOWN : A[i+1][j]
+        if Array[i + 1][j] != 1:  # Not a WALL cell
+            L.append((i + 1, j))
     return L
 
 
-def dfs():
-    pass
+def DepthFirstSearch(array, start, finish):
+    """
+    Returns the path if exists. [START, ... , FINISH]\n
+    If the path does not exist returns a empty list (len = 0)\n
+
+    -Some notes for DFS
+        * DFS does not guarantees that the solution will be optimal
+
+
+    :type array: np.Array
+    :type start: tuple
+    :type finish: tuple
+    :rtype: List
+    """
+    rootNode = Node(start)
+    closedSet = set()
+    searchFront = [rootNode]  # Βάλε την αρχική κατάσταση στο μέτωπο της αναζήτησης.
+    finishNode = None
+    while len(searchFront) != 0:  # Αν το μέτωπο της αναζήτησης είναι κενό τότε σταμάτησε.
+        currentNode = searchFront.pop(0)
+        if currentNode.data not in closedSet:  # Αν η κατάσταση ανήκει στο κλειστό σύνολο τότε πήγαινε στο while.
+
+            if currentNode.data == finish:  # Αν η κατάσταση είναι μία από τις τελικές, τότε ανέφερε τη λύση
+                # print("Found : ", currentNode.data)
+                finishNode = currentNode
+                break  # Αν θέλεις και άλλες λύσεις πήγαινε στο βήμα 2. Αλλιώς σταμάτησε.
+            else:  # Εφάρμοσε τους τελεστές μετάβασης για να βρεις τις καταστάσεις-παιδιά.
+                for item in getValidStates(array, currentNode.data):
+                    child = Node(item, currentNode)
+                    currentNode.add_child(child)
+                    searchFront.insert(0, child)  # Βάλε τις καταστάσεις-παιδιά στην αρχή του μετώπου της αναζήτησης.
+            closedSet.add(currentNode.data)  # Βάλε την κατάσταση-γονέα στο κλειστό σύνολο.
+    # backtracking
+    if finishNode is None:
+        print("No Solution Found!")
+        return []
+    else:
+        path = []
+        print("The solution is : ")
+        currentNode = finishNode
+        while currentNode != rootNode:
+            print("\t", currentNode.data)
+            currentNode = currentNode.parents[0]
 
 
 if __name__ == '__main__':
     A, START, FINISH = readImage2Array()
-    COLUMNS = len(A)
-    ROWS = len(A[0])
-    plotMaze()
-    rootNode = Node(START)
-    # getValidStates(0)
+    ROWS = len(A)
+    COLUMNS = len(A[0])
+    plotMaze(A)
+    Def
---
+++ pyUnitTest.py
@@ -0,0 +1,25 @@
+import unittest
+import Maze as mz
+import numpy as np
+
+
+class MyTestCase(unittest.TestCase):
+
+    def testValidStates(self):  # test extreme cases
+        A = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 1, 1],
+                      [0, 1, 0, 0, 1, 2, 0, 0, 0, 0],
+                      [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 1, 1, 0, 0, 0, 0, 3],
+                      [0, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+                      [0, 1, 0, 0, 0, 1, 0, 0, 0, 0]])
+
+        self.assertEqual(mz.getValidStates(A, (3, 1)), [(3, 0), (3, 2)])
+        self.assertEqual(mz.getValidStates(A, (5, 9)), [(5, 8)])
+        self.assertEqual(mz.getValidStates(A, (0, 0)), [(0, 1), (1, 0)])
+        self.assertEqual(mz.getValidStates(A, (5, 0)), [(4, 0)])
+        self.assertEqual(mz.getValidStates(A, (0, 9)), [(1, 9)])
+        self.assertEqual(mz.getValidStates(A, (2, 5)), [(2, 4), (1, 5), (2, 6), (3, 5)])
+        self.assertEqual(mz.getValidStates(A, (1, 5)), [(1, 6), (2, 5)])
+
+if __name__ == '__main__':
+    unittest.main()
